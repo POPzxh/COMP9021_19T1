fbf850c8c7356b6386c9f36b311f2a6d2ebf8d5b
Yina <48152350+tiancaiji@users.noreply.github.com>
2019-03-08 12:37:04 +1100

    add Matin test

--- quiz_2.py
+++ quiz_2.py
@@ -21,6 +21,41 @@ def describe_rule(rule_nb):
     After 0 followed by 1, we draw 1
     After 1 followed by 0, we draw 0
     After 1 followed by 1, we draw 0
+    >>> describe_rule(0)
+    The rule encoded by 0 is:  {(0, 0): 0, (0, 1): 0, (1, 0): 0, (1, 1): 0}
+    <BLANKLINE>
+    After 0 followed by 0, we draw 0
+    After 0 followed by 1, we draw 0
+    After 1 followed by 0, we draw 0
+    After 1 followed by 1, we draw 0
+    >>> describe_rule(5)
+    The rule encoded by 5 is:  {(0, 0): 0, (0, 1): 1, (1, 0): 0, (1, 1): 1}
+    <BLANKLINE>
+    After 0 followed by 0, we draw 0
+    After 0 followed by 1, we draw 1
+    After 1 followed by 0, we draw 0
+    After 1 followed by 1, we draw 1
+    >>> describe_rule(7)
+    The rule encoded by 7 is:  {(0, 0): 0, (0, 1): 1, (1, 0): 1, (1, 1): 1}
+    <BLANKLINE>
+    After 0 followed by 0, we draw 0
+    After 0 followed by 1, we draw 1
+    After 1 followed by 0, we draw 1
+    After 1 followed by 1, we draw 1
+    >>> describe_rule(10)
+    The rule encoded by 10 is:  {(0, 0): 1, (0, 1): 0, (1, 0): 1, (1, 1): 0}
+    <BLANKLINE>
+    After 0 followed by 0, we draw 1
+    After 0 followed by 1, we draw 0
+    After 1 followed by 0, we draw 1
+    After 1 followed by 1, we draw 0
+    >>> describe_rule(15)
+    The rule encoded by 15 is:  {(0, 0): 1, (0, 1): 1, (1, 0): 1, (1, 1): 1}
+    <BLANKLINE>
+    After 0 followed by 0, we draw 1
+    After 0 followed by 1, we draw 1
+    After 1 followed by 0, we draw 1
+    After 1 followed by 1, we draw 1
     '''
     rule = rule_encoded_by(rule_nb)
     print('The rule encoded by', rule_nb, 'is: ', rule)
@@ -39,7 +74,24 @@ def draw_line(rule_nb, first, second, length):
     that starts with "first" if "length" is at least equal to 1,
     followed by "second" if "length" is at least equal to 2,
     and with the remaining "length" - 2 0's and 1's determined by "rule_nb".
-
+    >>> draw_line(1, 0, 0, 8)
+    00000000
+    >>> draw_line(0, 0, 1, 9)
+    010000000
+    >>> draw_line(5, 1, 0, 10)
+    1000000000
+    >>> draw_line(6, 1, 1, 11)
+    11011011011
+    >>> draw_line(7, 0, 1, 12)
+    011111111111
+    >>> draw_line(10, 1, 0, 13)
+    1010101010101
+    >>> draw_line(11, 0, 0, 14)
+    00101010101010
+    >>> draw_line(12, 1, 1, 15)
+    110011001100110
+    >>> draw_line(15, 0, 1, 16)
+    0111111111111111
     >>> draw_line(3, 0, 0, 0)
     <BLANKLINE>
     >>> draw_line(3, 0, 0, 1)
@@ -103,6 +155,22 @@ def uniquely_produced_by_rule(line):
     -1
     >>> uniquely_produced_by_rule('1011011011011011011011')
     -1
+    >>> print((uniquely_produced_by_rule('100110'), uniquely_produced_by_rule('10011')))
+    (12, -1)
+    >>> print((uniquely_produced_by_rule('110011'), uniquely_produced_by_rule('11010101')))
+    (12, -1)
+    >>> print((uniquely_produced_by_rule('11001100110011001'), uniquely_produced_by_rule('1010101')))
+    (12, -1)
+    >>> print((uniquely_produced_by_rule('0011011'), uniquely_produced_by_rule('10110')))
+    (14, -1)
+    >>> print((uniquely_produced_by_rule('001101101101101101101'), uniquely_produced_by_rule('001101101101101101111')))
+    (14, -1)
+    >>> print((uniquely_produced_by_rule('011000'), uniquely_produced_by_rule('11000')))
+    (4, -1)
+    >>> print((uniquely_produced_by_rule('0110000000000'), uniquely_produced_by_rule('0110000001100')))
+    (4, -1)
+    >>> print((uniquely_produced_by_rule('011000'), uniquely_produced_by_rule('0000110')))
+    (4, -1)
     '''
     rule = dict()
     for i in range(len(line)-2):
